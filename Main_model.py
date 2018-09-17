38ac38008d2017046acecec7db45f47016bc69c8
Sedar Olmez <k1462369@kcl.ac.uk>
2018-09-17 15:01:34 +0100

    Both agent 1 and agent 2 can move to random positions in space!

--- Main_model.py
+++ Main_model.py
@@ -10,10 +10,29 @@ import random as random_number
 # Variables:
 y0 = 50
 x0 = 50
+y1 = 50
+x1 = 50
 
-if random_number.random() < 0.5:
-	y0 = y0 + 1
-else:
-	y0 = y0 - 1
+#Methods: 
+def generate_position(value1):
+	if random_number.random() < 0.5:
+		value1 = value1 + 1
+	else:
+		value1 = value1 - 1
+	return value1
 
-#print (y0)
+#Main: 
+print("Agent 1: Position of y: " + str(generate_position(y0)))
+print("Agent 1: Position of y: " + str(generate_position(y0)))
+
+
+print("Agent 1: Position of x: " + str(generate_position(x0)))
+print("Agent 1: Position of x: " + str(generate_position(x0)))
+
+
+print("Agent 2: Position of y: " + str(generate_position(y1)))
+print("Agent 2: Position of y: " + str(generate_position(y1)))
+
+
+print("Agent 2: Position of x: " + str(generate_position(x1)))
+print("Agent 2: Position of x: " + str(generate_position(x1)))
