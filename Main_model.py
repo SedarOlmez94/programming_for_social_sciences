5af7e4d6223011fe5f40aba1ed435d1a82ee111d
Sedar Olmez <k1462369@kcl.ac.uk>
2018-09-17 14:44:10 +0100

    Implemented a basic Model environment for agents

---
+++ Main_model.py
@@ -0,0 +1,19 @@
+import random as random_number
+# Algorithm to be implemented: 
+
+# Make a y variable.
+# Make a x variable.
+# Change y and x based on random numbers.
+# Make a second set of y and xs, and make these change randomly as well.
+# Work out the distance between the two sets of y and xs.
+
+# Variables:
+y0 = 50
+x0 = 50
+
+if random_number.random() < 0.5:
+	y0 = y0 + 1
+else:
+	y0 = y0 - 1
+
+#print (y0)
