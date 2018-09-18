0685ad9369315d2214ed353d23cbb360abf6dd3b
Sedar Olmez <k1462369@kcl.ac.uk>
2018-09-18 13:45:13 +0100

    The torus application of the plain was implemented, now agents will never go off the plain.

--- Main_model.py
+++ Main_model.py
@@ -1,50 +1,44 @@
-#Come up with a more genius solution to painting the furthest east agent red! 18/09/2018
 import random as random_number
 import numpy as np
-from scipy.spatial import distance
 import operator as op
 import matplotlib.pyplot
+from scipy.spatial import distance
 
 # Variables:
 agents = []
 number_of_agents = 10
+number_of_iterations = 100
 for i in range(number_of_agents):
 	agents.append([random_number.randint(0, 99), random_number.randint(0, 99)])
 furthest_east_agent = []
 case_check = False
 pointer = 0
 
-#Methods:
-def generate_position(value1):
+''' The for loop ensures a torus around the plain, that way agents always appear
+within plain.'''
+for i in range(number_of_agents):
 	if random_number.random() < 0.5:
-		value1 = value1 + 1
+		agents[i][0] = (agents[i][0] + 1) % 100
 	else:
-		value1 = value1 - 1
-	return value1
+		agents[i][0] = (agents[i][0] - 1) % 100
 
 
-#Calculating the euclidean distance between two agents, I run the distance over 100 times for various results
 
+#Calculating the euclidean distance between the agents, I run the distance over 100 times for experimentation.
 while case_check != True:
-	diff_y_coor = distance.euclidean(generate_position(agents[0][0]), generate_position(agents[1][0]))
-	diff_x_coor = distance.euclidean(generate_position(agents[0][1]), generate_position(agents[1][1]))
+	diff_y_coor = distance.euclidean(agents[0][0], agents[1][0])
+	diff_x_coor = distance.euclidean(agents[0][1], agents[1][1])
 	answer = (diff_y_coor + diff_x_coor)**0.5
 	print(answer)
 	pointer = pointer + 1
-	if pointer == 100:
+	if pointer == number_of_iterations:
 		case_check = True
-print (agents)
+
+#Here we plot the results of the agent's destination on the plain, the east most agent is painted red.
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
 for i in range(number_of_agents-1):
 	matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
-	#matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
 furthest_east_agent = max(agents)
 matplotlib.pyplot.scatter(furthest_east_agent[0], furthest_east_agent[1], color='red')
 matplotlib.pyplot.show()
-# print(max(agents, key=op.itemgetter(1)))
-# print (agents)
-# print (max(agents))
-# answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5 could have been used.
-#for x in range(101):
-#	print (answer)
